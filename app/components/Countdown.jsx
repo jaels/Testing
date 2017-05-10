d0a45a060889635bc0c80e0e4600ef8fbce0dd88
Yael Stein <yaelst@gmail.com>
2017-05-10 15:13:07 +0200

    timer and more tests

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var Clock = require('Clock');
+
+var Countdown = React.createClass({
+render: function() {
+    return <Clock totalSeconds={129}/>
+}
+})
+
+module.exports = Countdown;
